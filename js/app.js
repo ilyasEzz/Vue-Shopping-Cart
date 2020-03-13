3b50a6f6df9696db9921e5f122cb48c0726e9b9a
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-03-13 13:41:28 +0700

    post request

--- js/app.js
+++ js/app.js
@@ -14,9 +14,33 @@ let f = async (path, method = 'get', data = [], useToken = true, form = false) =
         headers: {}
     }
 
+    if (!form) {
+        options.headers['Content-Type'] = `application/json`;
+        if (method === 'POST') {
+            console.log("Nethod POST");
+            options.body = JSON.stringify(data);
+            console.log("DATA STRINGIFIED");
+
+        }
+    } else {
+        let formdata = new FormData();
+
+        for (let name in data) {
+            formdata.append(name, data[name]);
+        }
+        options.body = formdata;
+    }
+
     let result = await fetch(`${api}${path}`, options);
 
-    return (method === "DELETE") ? true : await result.json();
+
+
+    if (method === "DELETE") {
+        return true;
+    }
+    else if (method === "POST") { return result; }
+    else { return await result.json(); }
+
 
 }
 
@@ -38,7 +62,14 @@ let app = new Vue({
                 }
             },
             products: [],
-            cart: [],
+            cart: [
+                {
+                    id: 4,
+                    name: "product 4",
+                    description: "Все последующие запросы требуют авторизации с использованием Bearer-токена.",
+                    price: 10000
+                },
+            ],
             showCart: false
         }
     },
@@ -53,9 +84,31 @@ let app = new Vue({
         },
         async addToCart(id) {
             let product = this.products.find((product) => product.id === id);
-            this.cart.push(product);
-            this.cart = [...new Set(this.cart)]
+            let productDb = await f(`/products/${id}`, 'get');
 
+            // Check if the product is in the Database
+            this.cart.push(product);
+            this.cart = [...new Set(this.cart)];
+            let allCarts = await f('/cart', 'get');
+            let lastIndex = 0;
+
+            if (allCarts.length < 1) {
+                lastIndex = allCarts[allCarts.length - 1].id;
+            }
+            const newCartDb = {
+                id: lastIndex + 1,
+                user_id: this.user.id,
+                product: [...this.cart],
+
+            };
+            console.log(newCartDb)
+            // POST newCart
+            await f('/cart', 'post', newCartDb)
+
+        },
+        removeFromCart(id) {
+            this.cart = this.cart.filter(product => product.id !== id);
         }
+
     }
 })
